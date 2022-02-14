46035967fe74ccd57c394150b6bcd6caacd99488
Gershon Koral <gershi.koral@dariel.co.za>
2022-02-14 10:21:03 +0200

    Commit Xamerin

---
+++ parking-finder/parking-finder/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using parking_finder.ViewModels;
+using parking_finder.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace parking_finder
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ parking-finder/parking-finder/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using parking_finder.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace parking_finder.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
