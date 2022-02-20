8da07546a7ae76f710c9a1566b05934967477f52
Gerald Kirui <gerald.kirui@dariel.co.za>
2022-02-20 21:28:33 +0200

    Setup function in database service to update parking space coordinates

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  final String pid;
+  DatabaseService(this.pid);
+
   final CollectionReference parkingSpaceCollection =
       FirebaseFirestore.instance.collection('parking_spaces');
+
+  Future updateParkingData(String coordinates, String state) async {
+    return await parkingSpaceCollection
+        .doc(pid)
+        .set({'coordinates': coordinates, 'state': state});
+  }
 }
